f52f3a6f6e353ae0d66ac4907fd84e9c6690bfbd
zacker330 <zacker330@gmail.com>
2021-08-22 18:19:37 +0800

    init

---
+++ src/main/java/codes/showme/example/jvmoom/Main.java
@@ -0,0 +1,26 @@
+package codes.showme.example.jvmoom;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        int dummyArraySize = 15;
+        System.out.println("Max JVM memory: " + Runtime.getRuntime().maxMemory());
+        long memoryConsumed = 0;
+        try {
+            long[] memoryAllocated = null;
+            for (int loop = 0; loop < Integer.MAX_VALUE; loop++) {
+                memoryAllocated = new long[dummyArraySize];
+                memoryAllocated[0] = 0;
+                memoryConsumed += dummyArraySize * Long.SIZE;
+                System.out.println("Memory Consumed till now: " + memoryConsumed);
+                dummyArraySize *= dummyArraySize * 2;
+                Thread.sleep(500);
+            }
+        } catch (OutOfMemoryError outofMemory) {
+            System.out.println("Catching out of memory error");
+            //Log the information,so that we can generate the statistics (latter on).
+            throw outofMemory;
+        }
+    }
+}
+
